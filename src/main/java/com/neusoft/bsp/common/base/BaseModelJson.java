eaaee1266776c6c46e2a3cb5d3723f1eff2a8f2e
lupang111 <46068388+lupang111@users.noreply.github.com>
2020-07-02 17:33:16 +0800

    bsp前端
    新增心愿单，点击商品图像会跳转至商品详情页面

---
+++ src/main/java/com/neusoft/bsp/common/base/BaseModelJson.java
@@ -0,0 +1,17 @@
+package com.neusoft.bsp.common.base;
+
+public class BaseModelJson<T> extends BaseModel {
+
+    public T data;
+
+    @Override
+    public String toString() {
+        return "BaseModelJson{" +
+                "data=" + data +
+                ", code=" + code +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    //result.data
+}
